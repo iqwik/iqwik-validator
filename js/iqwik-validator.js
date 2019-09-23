1776a536b6d488f3438b8d8c35d0703d03c4e4c3
Artem Zubarev <44653536+iqwik@users.noreply.github.com>
2019-09-23 17:40:42 +0300

    Update iqwik-validator.js

--- js/iqwik-validator.js
+++ js/iqwik-validator.js
@@ -1,70 +1,77 @@
-/* author: Artem Zubarev
- *  git: https://github.com/iqwik/iqwik-validator/
- */
 (function($){
     $.fn.iqwikValidator = function(options) {
         var settings = $.extend( {
-            text        : /^([\w|\d]|\s){2,}$/i,
-            name        : /^[а-яА-ЯёЁa-zA-Z]{2,}$/i,
-            phone       : /^\+7[-]\d{3}[-]\d{3}[-]\d{4}$/,
+            text        : /^([\S]{1,}).+/i,
+            name        : /^([а-яА-ЯёЁa-zA-Z]){2,}$/i,
+            phone       : /^\+7[(]\d{3}[)]\d{3}[-]\d{2}[-]\d{2}$/,
             email       : /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.(org|com|co|net|io|ru)$/i,
-            errorClass  : true
+            errorClass  : true,
+            onSubmit    : undefined
         }, options);
-        return this.each(function(){
-            var errors = [];
-            var checkField = function(){
-                var result = true;
-                var $this = $(this);
-                var type = $this.data('type');
-                var pattern = typeof type !== 'undefined' && typeof settings[type] !== 'undefined' ? settings[type] : settings.text;
-                var errorText = $this.data('err-text');
-                if($this.data('req') === true) {
-                    result = false;
-                    if(!pattern.test($this.val())){
-                        if(settings.errorClass === true) {
-                            $this.addClass('error-border');
-                            if ($this.siblings('.error').length < 1) {
-                                $('<span class="error">' + errorText + '</span>').insertAfter($this);
-                            } else {
-                                $this.siblings('.error').html(errorText);
-                            }
+        var errors = [];
+        var checkField = function(){
+            var result = true;
+            var $this = $(this);
+            var type = $this.data('type');
+            var pattern = typeof type !== 'undefined' && typeof settings[type] !== 'undefined' ? settings[type] : settings.text;
+            var errorText = $this.data('err-text');
+            if($this.data('req') === true) {
+                result = false;
+                if(!pattern.test($this.val())){
+                    if(settings.errorClass === true) {
+                        $this.addClass('error-border');
+                        if ($this.siblings('.error').length < 1) {
+                            $('<span class="error">' + errorText + '</span>').insertAfter($this);
+                        } else {
+                            $this.siblings('.error').html(errorText);
+                        }
 
-                        } else {
-                            var name = $this.attr('name');
-                            var $data = $('[data-err-class="'+name+'"]');
-                            $data.addClass('error-border');
-                            if ($data.siblings('.error').length < 1) {
-                                $('<div class="error">' + errorText + '</div>').insertAfter($data);
-                            } else {
-                                $data.siblings('.error').html(errorText);
-                            }
-                        }
-                        if(errors.indexOf(errorText) === -1) {
-                            errors.push(errorText);
-                        }
                     } else {
-                        if(settings.errorClass === true) {
-                            $this.removeClass('error-border');
-                            $this.siblings('.error').remove();
+                        var name = $this.attr('name');
+                        var $data = $('[data-err-class="'+name+'"]');
+                        $data.addClass('error-border');
+                        if ($data.siblings('.error').length < 1) {
+                            $('<div class="error">' + errorText + '</div>').insertAfter($data);
                         } else {
-                            name = $this.attr('name');
-                            $data = $('[data-err-class="'+name+'"]');
-                            $data.removeClass('error-border');
-                            $data.siblings('.error').remove();
+                            $data.siblings('.error').html(errorText);
                         }
-                        if(errors.indexOf(errorText) !== -1){
-                            errors.splice(errors.indexOf(errorText),1);
-                        }
-                        result = true;
                     }
+                    if(errors.indexOf(errorText) === -1) {
+                        errors.push(errorText);
+                    }
+                } else {
+                    if(settings.errorClass === true) {
+                        $this.removeClass('error-border');
+                        $this.siblings('.error').remove();
+                    } else {
+                        name = $this.attr('name');
+                        $data = $('[data-err-class="'+name+'"]');
+                        $data.removeClass('error-border');
+                        $data.siblings('.error').remove();
+                    }
+                    if(errors.indexOf(errorText) !== -1){
+                        errors.splice(errors.indexOf(errorText),1);
+                    }
+                    result = true;
                 }
-                return result;
-            };
-            $(this).on('focus', 'input, textarea', checkField);
-            $(this).on('keyup', 'input, textarea', checkField);
-            $(this).on('submit', function(){
+            }
+            return result;
+        };
+
+        return this.each(function(){
+            $(this).on('focus', 'input[type="text"],input[type="email"],input[type="number"], textarea', checkField);
+            $(this).on('keyup', 'input[type="text"],input[type="email"],input[type="number"], textarea', checkField);
+            $(this).on('change', 'input[type="checkbox"]', checkField);
+            $(this).on('submit', function(e){
+                e.preventDefault();
                 $(this).find('input, textarea').map(checkField);
-                if(errors.length > 0) { return false; }
+                if(errors.length > 0) {
+                    return;
+                }
+
+                if(typeof settings.onSubmit !== 'undefined') {
+                    settings.onSubmit(e.target);
+                }
             });
         });
     };
