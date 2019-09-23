ee407f7856f9124c2c6f6ff4e56e603a7617b5d6
Artem Zubarev <44653536+iqwik@users.noreply.github.com>
2019-09-23 11:31:26 +0300

    Update iqwik-validator.js

--- js/iqwik-validator.js
+++ js/iqwik-validator.js
@@ -4,10 +4,11 @@
 (function($){
     $.fn.iqwikValidator = function(options) {
         var settings = $.extend( {
-            text     : /^([\w|\d]|\s){2,}$/i,
-            name     : /^[а-яА-ЯёЁa-zA-Z]{2,}$/i,
-            phone    : /^\+7[-]\d{3}[-]\d{3}[-]\d{4}$/,
-            email    : /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.(org|com|co|net|io|ru)$/i
+            text        : /^([\w|\d]|\s){2,}$/i,
+            name        : /^[а-яА-ЯёЁa-zA-Z]{2,}$/i,
+            phone       : /^\+7[-]\d{3}[-]\d{3}[-]\d{4}$/,
+            email       : /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.(org|com|co|net|io|ru)$/i,
+            errorClass  : true
         }, options);
         return this.each(function(){
             var errors = [];
@@ -20,18 +21,37 @@
                 if($this.data('req') === true) {
                     result = false;
                     if(!pattern.test($this.val())){
-                        $this.addClass('error-border');
-                        if ($this.siblings('.error').length < 1) {
-                            $('<span class="error">' + errorText + '</span>').insertAfter($this);
+                        if(settings.errorClass === true) {
+                            $this.addClass('error-border');
+                            if ($this.siblings('.error').length < 1) {
+                                $('<span class="error">' + errorText + '</span>').insertAfter($this);
+                            } else {
+                                $this.siblings('.error').html(errorText);
+                            }
+
                         } else {
-                            $this.siblings('.error').html(errorText);
+                            var name = $this.attr('name');
+                            var $data = $('[data-err-class="'+name+'"]');
+                            $data.addClass('error-border');
+                            if ($data.siblings('.error').length < 1) {
+                                $('<div class="error">' + errorText + '</div>').insertAfter($data);
+                            } else {
+                                $data.siblings('.error').html(errorText);
+                            }
                         }
                         if(errors.indexOf(errorText) === -1) {
                             errors.push(errorText);
                         }
                     } else {
-                        $this.removeClass('error-border');
-                        $this.siblings('.error').remove();
+                        if(settings.errorClass === true) {
+                            $this.removeClass('error-border');
+                            $this.siblings('.error').remove();
+                        } else {
+                            name = $this.attr('name');
+                            $data = $('[data-err-class="'+name+'"]');
+                            $data.removeClass('error-border');
+                            $data.siblings('.error').remove();
+                        }
                         if(errors.indexOf(errorText) !== -1){
                             errors.splice(errors.indexOf(errorText),1);
                         }
